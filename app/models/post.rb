df37d6f04d8cc4714911322536e945db1805fe94
neil patel <neil@myindianweddingapp.com>
2015-02-08 10:47:07 +0000

    Added conditional routes

---
+++ app/models/post.rb
@@ -0,0 +1,4 @@
+class Post < ActiveRecord::Base
+
+	belongs :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150208100012) do
+ActiveRecord::Schema.define(version: 20150208102622) do
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
